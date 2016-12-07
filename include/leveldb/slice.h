4d3821ef71a404ee7d5a15c509dc7239fa8b9fe9
Kevins Bobo <kevins.bobo@gmail.com>
2016-12-07 18:00:35 +0800

    add remark to slice.h & status.h

--- include/leveldb/slice.h
+++ include/leveldb/slice.h
@@ -7,10 +7,16 @@
 // is not used after the corresponding external storage has been
 // deallocated.
 //
+// Slice是一个简单的结构，它包含了一个指向外部存储的指针和一个大小值。
+// Slice的使用者必须确保Slice指向的外部存储没有被销毁。
+//
 // Multiple threads can invoke const methods on a Slice without
 // external synchronization, but if any of the threads may call a
 // non-const method, all threads accessing the same Slice must use
 // external synchronization.
+//
+// 多线程可以在Slice上调用const方法而不依赖其他的外部同步，但如果一个线程
+// 调用的是non-const方法，那么其他线程访问这个Slice时就必须使用外部同步。
 
 #ifndef STORAGE_LEVELDB_INCLUDE_SLICE_H_
 #define STORAGE_LEVELDB_INCLUDE_SLICE_H_
@@ -24,6 +30,7 @@ namespace leveldb {
 
 class Slice {
  public:
+  // 针对不同类型参数的构造函数
   // Create an empty slice.
   Slice() : data_(""), size_(0) { }
 
@@ -37,25 +44,32 @@ class Slice {
   Slice(const char* s) : data_(s), size_(strlen(s)) { }
 
   // Return a pointer to the beginning of the referenced data
+  // 返回一个指向引用的数据开头的指针
   const char* data() const { return data_; }
 
   // Return the length (in bytes) of the referenced data
+  // 返回引用的数据的长度
   size_t size() const { return size_; }
 
   // Return true iff the length of the referenced data is zero
+  // 如果数据的长度为0则返回ture
   bool empty() const { return size_ == 0; }
 
   // Return the ith byte in the referenced data.
+  // 重载[]操作符，返回引用数据中第n个下标指向的数据
   // REQUIRES: n < size()
+  // 警告：n必须小于size()的返回值
   char operator[](size_t n) const {
     assert(n < size());
     return data_[n];
   }
 
   // Change this slice to refer to an empty array
+  // 将这个Slice指向一个内容为空的数组
   void clear() { data_ = ""; size_ = 0; }
 
   // Drop the first "n" bytes from this slice.
+  // 丢弃data中的前n个数据内容
   void remove_prefix(size_t n) {
     assert(n <= size());
     data_ += n;
@@ -63,15 +77,18 @@ class Slice {
   }
 
   // Return a string that contains the copy of the referenced data.
+  // 将data中的内容作为string类型返回
   std::string ToString() const { return std::string(data_, size_); }
 
   // Three-way comparison.  Returns value:
+  // 比较函数，有三种返回方式代表不同的结果
   //   <  0 iff "*this" <  "b",
   //   == 0 iff "*this" == "b",
   //   >  0 iff "*this" >  "b"
   int compare(const Slice& b) const;
 
   // Return true iff "x" is a prefix of "*this"
+  // x的内容是当前对象的内容的前缀时返回ture
   bool starts_with(const Slice& x) const {
     return ((size_ >= x.size_) &&
             (memcmp(data_, x.data_, x.size_) == 0));
@@ -84,6 +101,7 @@ class Slice {
   // Intentionally copyable
 };
 
+// 重载==和!=运算符
 inline bool operator==(const Slice& x, const Slice& y) {
   return ((x.size() == y.size()) &&
           (memcmp(x.data(), y.data(), x.size()) == 0));
--- include/leveldb/status.h
+++ include/leveldb/status.h
@@ -5,10 +5,16 @@
 // A Status encapsulates the result of an operation.  It may indicate success,
 // or it may indicate an error with an associated error message.
 //
+// 封装了一个操作完成后返回的状态信息。它可以表示执行成功，也可以表示执行失败
+// 并包含一条错误信息。
+//
 // Multiple threads can invoke const methods on a Status without
 // external synchronization, but if any of the threads may call a
 // non-const method, all threads accessing the same Status must use
 // external synchronization.
+//
+// 多线程可以调用一个Status的const方法，而不必使用外部同步。但如果一个线程调用
+// 的是non-const方法，那其他线程想要访问这个相同的Status就必须使用外部同步。
 
 #ifndef STORAGE_LEVELDB_INCLUDE_STATUS_H_
 #define STORAGE_LEVELDB_INCLUDE_STATUS_H_
@@ -21,17 +27,21 @@ namespace leveldb {
 class Status {
  public:
   // Create a success status.
+  // 创建一个表示操作成功的Status
   Status() : state_(NULL) { }
   ~Status() { delete[] state_; }
 
   // Copy the specified status.
+  // 拷贝一个特定的Status
   Status(const Status& s);
   void operator=(const Status& s);
 
   // Return a success status.
+  // 返回一个表示操作成功的Status
   static Status OK() { return Status(); }
 
   // Return error status of an appropriate type.
+  // 返回适当的错误Status类型
   static Status NotFound(const Slice& msg, const Slice& msg2 = Slice()) {
     return Status(kNotFound, msg, msg2);
   }
@@ -49,30 +59,40 @@ class Status {
   }
 
   // Returns true iff the status indicates success.
+  // 返回操作的执行结果，操作成功返回ture
   bool ok() const { return (state_ == NULL); }
 
   // Returns true iff the status indicates a NotFound error.
+  // 如果是NotFound错误就返回ture
   bool IsNotFound() const { return code() == kNotFound; }
 
   // Returns true iff the status indicates a Corruption error.
+  // 如果是Corruption错误就返回ture
   bool IsCorruption() const { return code() == kCorruption; }
 
   // Returns true iff the status indicates an IOError.
+  // 如果是IOError就返回ture
   bool IsIOError() const { return code() == kIOError; }
 
   // Returns true iff the status indicates a NotSupportedError.
+  // 如果是NotSupported错误就返回ture
   bool IsNotSupportedError() const { return code() == kNotSupported; }
 
   // Returns true iff the status indicates an InvalidArgument.
+  // 如果是InvalidArgument就返回ture
   bool IsInvalidArgument() const { return code() == kInvalidArgument; }
 
   // Return a string representation of this status suitable for printing.
+  // 返回一个适合打印的string类型的状态说明
   // Returns the string "OK" for success.
+  // 如果返回的是"OK"说明执行成功
   std::string ToString() const;
 
  private:
   // OK status has a NULL state_.  Otherwise, state_ is a new[] array
   // of the following form:
+  // OK状态包含一个空的state_指针。不然的话state_指针指向的是一个
+  // 新的数组，该数组包含的内容如下：
   //    state_[0..3] == length of message
   //    state_[4]    == code
   //    state_[5..]  == message
@@ -91,6 +111,7 @@ class Status {
     return (state_ == NULL) ? kOk : static_cast<Code>(state_[4]);
   }
 
+
   Status(Code code, const Slice& msg, const Slice& msg2);
   static const char* CopyState(const char* s);
 };
@@ -98,9 +119,13 @@ class Status {
 inline Status::Status(const Status& s) {
   state_ = (s.state_ == NULL) ? NULL : CopyState(s.state_);
 }
+
+// 重载赋值操作符
 inline void Status::operator=(const Status& s) {
   // The following condition catches both aliasing (when this == &s),
+  // 下面的判断条件也捕获两个别名，即（当 this == &s），
   // and the common case where both s and *this are ok.
+  // 通常用于s和*this都是ok的情况下。
   if (state_ != s.state_) {
     delete[] state_;
     state_ = (s.state_ == NULL) ? NULL : CopyState(s.state_);
